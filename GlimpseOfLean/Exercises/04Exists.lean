9f7a03d8672ba1bbcdb7ab0a19f37906723e6f1d
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-03-20 20:27:10 +0800

    Learn more a bout a tactic `tauto`

--- GlimpseOfLean/Exercises/04Exists.lean
+++ GlimpseOfLean/Exercises/04Exists.lean
@@ -40,7 +40,9 @@ example (p q r s : Prop) (h : p → r) (h' : q → s) : p ∧ q → r ∧ s := b
 /- You can choose your own style in the next exercise. -/
 
 example (p q r : Prop) : (p → (q → r)) ↔ p ∧ q → r := by {
-  sorry
+  apply Iff.intro --constructor
+  tauto
+  tauto
 }
 
 /- Of course Lean doesn't need any help to prove this kind of logical tautologies.
